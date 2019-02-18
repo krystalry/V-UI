260e3ae5ab6cb9458b15d1d737f1167fa4b8e6f9
krystalry <2745147939@qq.com>
2019-02-18 17:34:16 +0800

    加入  parcel 将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+    }
+</style>
+
+<script>
+    export default{}
+</script>
