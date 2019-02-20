a8aeeb37a528bf42f5b58553feb7eecda7dace37
krystalry <2745147939@qq.com>
2019-02-20 20:40:45 +0800

    添加 loading  icon图标

--- src/button.vue
+++ src/button.vue
@@ -1,23 +1,31 @@
 <template>
     <!--方法1：-->
     <!--<button class="g-button" v-if="iconPosition === 'right'">-->
-        <!--<slot></slot>-->
-        <!--<svg v-if="icon" class="icon">-->
-            <!--<use :xlink:href=`#i-${icon}`></use>-->
-        <!--</svg>-->
+    <!--<slot></slot>-->
+    <!--<svg v-if="icon" class="icon">-->
+    <!--<use :xlink:href=`#i-${icon}`></use>-->
+    <!--</svg>-->
     <!--</button>-->
     <!--<button class="g-button" v-else>-->
-        <!--<svg v-if="icon" class="icon">-->
-            <!--<use :xlink:href=`#i-${icon}`></use>-->
-        <!--</svg>-->
-        <!--<slot></slot>-->
+    <!--<svg v-if="icon" class="icon">-->
+    <!--<use :xlink:href=`#i-${icon}`></use>-->
+    <!--</svg>-->
+    <!--<slot></slot>-->
     <!--</button>-->
     <!--方法2：-->
+    <!--<button class="g-button" :class="{[`icon-${iconPosition}`]:true}">-->
+    <!--<slot></slot>-->
+    <!--<svg v-if="icon" class="icon">-->
+    <!--<use :xlink:href=`#i-${icon}`></use>-->
+    <!--</svg>-->
+    <!--</button>-->
+    <!--icon.vue版-->
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <slot></slot>
-        <svg v-if="icon" class="icon">
-            <use :xlink:href=`#i-${icon}`></use>
-        </svg>
+        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 </template>
 
@@ -29,10 +37,10 @@
         props: {
             icon: {},
             iconPosition: {
-                type:String,
+                type: String,
                 default: 'left',
                 validator(value) {
-                    return value ==='left' || value ==='right'
+                    return value === 'left' || value === 'right'
                 }
             }
         }
@@ -40,24 +48,57 @@
 </script>
 
 <style lang="scss">
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
-        border-radius: var(--button-radius);
-        border-color: var(--button-color);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-    }
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
 
-    .g-button:hover {
-        border-color: var(--button-color-hower);
-    }
+     &:hover {
+        border-color: var(--border-color-hover);
+      }
 
-    .g-button:active {
+     &:active {
         background-color: var(--button-active-bg);
-    }
+      }
 
-    .g-button:focus {
+     &:focus {
         outline: none;
+      }
+      >.content {
+        order: 2;
+       }
+     >.icon {
+        order: 1;
+        margin-right: .1em;
+      }
+     &.icon-right {
+      >.content {
+        order: 1;
+       }
+      >.icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+       }
+    }
+     .loading {
+        animation: spin 2s infinite linear;
+     }
     }
 </style>
